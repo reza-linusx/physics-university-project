b751fe5d50d592f15c753d6f6651a61bf7566694
Reza <reza.linusx@gmail.com>
2026-08-02 12:28:29 +0330

    Clean up files and refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,8 @@
 import React from "react";
-import { Canvas } from "@react-three/fiber";
-import Globe from "./components/Globe";
+import Length from "./chapters/measurement/Length";
 
-function Length() {
-  return (
-    <div className="h-screen w-screen flex flex-col md:flex-row bg-slate-600 p-5 gap-2">
-      <div className="flex-1 md:flex-3 bg-slate-700 flex items-center justify-center rounded-lg">
-        <Canvas>
-          <ambientLight intensity={1} />
-          <directionalLight position={[5, 5, 5]} intensity={2.0} />
-          <directionalLight position={[-5, -5, -5]} intensity={0.5} />
-          <Globe />
-        </Canvas>
-      </div>
-
-      <div className="flex-1 md:flex-1 bg-slate-700 flex flex-col items-center justify-center px-8 rounded-lg">
-        <h1 className="text-white text-4xl font-bold mb-4">Length</h1>
-        <p className="text-slate-300 text-center">Information will go here</p>
-      </div>
-    </div>
-  );
+function App() {
+  return <Length />;
 }
 
-export default Length;
+export default App;
--- src/chapters/measurement/Length.jsx
+++ src/chapters/measurement/Length.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import Globe from "./Globe";
+import Globe from "../../components/Globe";
 
 function Length() {
   return (
