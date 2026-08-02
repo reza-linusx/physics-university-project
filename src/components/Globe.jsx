51ddccc6d0f2b0588fab9f32b272803f7d924a91
Reza <reza.linusx@gmail.com>
2026-08-02 11:40:58 +0330

    Add a simple rotation to the globe

--- src/components/Globe.jsx
+++ src/components/Globe.jsx
@@ -1,14 +1,22 @@
-import { useLoader } from "@react-three/fiber";
-import React from "react";
+import React, { useRef } from "react";
+import { useLoader, useFrame } from "@react-three/fiber";
 import { TextureLoader } from "three";
+import earthTexture from "../assets/earth.jpg";
 
 function Globe() {
-  const texture = useLoader(TextureLoader, "/src/assets/earth.jpg");
+  const meshRef = useRef();
+  const texture = useLoader(TextureLoader, earthTexture);
+
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.y += 0.002;
+    }
+  });
 
   return (
-    <mesh>
-      <sphereGeometry args={[1.8, 32, 32]} />
-      <meshStandardMaterial color="#e8705a" map={texture} />
+    <mesh ref={meshRef}>
+      <sphereGeometry args={[1.8, 64, 64]} />
+      <meshStandardMaterial map={texture} />
     </mesh>
   );
 }
