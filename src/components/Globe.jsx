82943932e2a75e7dac04ac2f40b12f2ff544f732
Reza <reza.linusx@gmail.com>
2026-08-02 11:38:50 +0330

    Map the picture of earth to the globe

--- src/components/Globe.jsx
+++ src/components/Globe.jsx
@@ -1,10 +1,14 @@
+import { useLoader } from "@react-three/fiber";
 import React from "react";
+import { TextureLoader } from "three";
 
 function Globe() {
+  const texture = useLoader(TextureLoader, "/src/assets/earth.jpg");
+
   return (
     <mesh>
       <sphereGeometry args={[1.8, 32, 32]} />
-      <meshStandardMaterial color="#e8705a" />
+      <meshStandardMaterial color="#e8705a" map={texture} />
     </mesh>
   );
 }
