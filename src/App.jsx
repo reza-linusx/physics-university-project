0497b941bb1e52d2d25fcea832add40611c41b17
Reza <reza.linusx@gmail.com>
2026-08-03 11:12:55 +0330

    Create whiteboard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Length from "./chapters/measurement/Length";
+import Whiteboard from "./components/Whiteboard";
 
 function App() {
-  return <Length />;
+  return (
+    <>
+      <Length />
+      <Whiteboard />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Whiteboard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Tldraw } from "tldraw";
+import "tldraw/tldraw.css";
+
+function Whiteboard() {
+  return (
+    <div className="w-full h-[400px] bg-slate-800 rounded-lg overflow-hidden p-1">
+      <div className="w-full h-full rounded-lg overflow-hidden">
+        <Tldraw />
+      </div>
+    </div>
+  );
+}
+
+export default Whiteboard;
