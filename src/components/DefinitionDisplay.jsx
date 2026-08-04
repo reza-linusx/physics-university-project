11a7047a985ce6650ce32fcbab83b98eae13f1db
Reza <reza.linusx@gmail.com>
2026-08-04 11:07:52 +0330

    Create defenition display component

---
+++ src/components/DefinitionDisplay.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { TEXT, CARD } from "../../../styles/tailwindStyles";
+
+function DefinitionDisplay({ title, content, note, rtl = false }) {
+  const contentClass = rtl
+    ? TEXT.DEFINITION_CONTENT_RTL
+    : TEXT.DEFINITION_CONTENT;
+  const noteClass = rtl ? TEXT.DEFINITION_NOTE_SMALL : TEXT.DEFINITION_NOTE;
+
+  return (
+    <div className="text-white text-center w-full">
+      <p className={TEXT.DEFINITION_TITLE}>{title}</p>
+      <div className={CARD.DEFINITION}>
+        <p className={contentClass}>{content}</p>
+      </div>
+      {note && <p className={noteClass}>{note}</p>}
+    </div>
+  );
+}
+
+export default DefinitionDisplay;
