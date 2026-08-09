c94270d52a726ca9121dc2be58adc23a6a36d663
Reza <reza.linusx@gmail.com>
2026-08-09 12:24:06 +0330

    Remove unused info

--- src/App.jsx
+++ src/App.jsx
@@ -12,17 +12,22 @@ import OverlayWhiteboard from "./components/OverlayWhiteboard";
 // context
 import { ViewContext } from "./context/ViewContext";
 
+// math
+import { MathJaxContext } from "better-react-mathjax";
+
 function App() {
   const { overlayActivated } = useContext(ViewContext);
   return (
-    <div className="min-h-screen bg-gray-800 text-white flex relative">
-      <Sidebar />
-      <div className="flex-1 relative p-5 max-w-screen h-screen overflow-hidden">
-        {overlayActivated && <OverlayWhiteboard />}
-        <Content />
+    <MathJaxContext>
+      <div className="min-h-screen bg-gray-800 text-white flex relative">
+        <Sidebar />
+        <div className="flex-1 relative p-5 max-w-screen h-screen overflow-hidden">
+          {overlayActivated && <OverlayWhiteboard />}
+          <Content />
+        </div>
+        <DefinitionPanel />
       </div>
-      <DefinitionPanel />
-    </div>
+    </MathJaxContext>
   );
 }
 
