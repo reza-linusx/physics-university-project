fcb80c64d357d97f234a4fd5ee4f16f4d3401326
Reza <reza.linusx@gmail.com>
2026-08-10 10:26:53 +0330

    Remove lighting from the waves

--- src/components/RepeatingWaveView.jsx
+++ src/components/RepeatingWaveView.jsx
@@ -6,9 +6,6 @@ import RepeatingWaves from "./RepeatingWaves";
 function RepeatingWaveView() {
   return (
     <Canvas camera={{ position: [0, 0, 5], fov: 40 }}>
-      <ambientLight intensity={0.8} />
-      <directionalLight position={[5, 5, 5]} intensity={0.5} />
-      <directionalLight position={[-5, -5, -5]} intensity={0.3} />
       <OrbitControls enableZoom={true} maxDistance={10} minDistance={2} />
       <RepeatingWaves />
     </Canvas>
