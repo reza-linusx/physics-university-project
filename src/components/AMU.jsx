2ba1301d4b7f675e9f5aa8f555faf989b448b0ca
Reza <reza.linusx@gmail.com>
2026-08-14 12:32:20 +0330

    Add GitHub Pages deployment

---
+++ src/components/AMU.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AMU() {
+  return (
+    <div className="w-full h-full flex items-center justify-center text-2xl">
+      <h1>تعریف بر اساس جرم اتم کربن ۱۲</h1>
+    </div>
+  );
+}
--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,5 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), viteSingleFile()],
+  base: "/physics-university-project/",
 });
